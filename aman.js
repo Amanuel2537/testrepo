0c04db581854b2bba98aa0c55ab967bb8ffecf31
Amanuel Samul <56428330+Amanuel2537@users.noreply.github.com>
2023-01-18 22:42:28 +0300

    Add files via upload
    New javascript file

---
+++ aman.js
@@ -0,0 +1,32 @@
+// function printMyInput(user_input){
+//     console.log("the game "+ user_input)
+// }
+
+// printMyInput("amanuel")
+
+
+// var total =10+1+"3"
+// console.log(total)
+
+
+var a = new String("Hello");
+
+var b = "Hello";
+
+if (a ===b){
+
+    window.alert("Same");
+
+}else{
+
+    window.alert("Different");
+
+}
+
+
+// numArray=new Array(1,2,3,4,5)
+// console.log(numArray.length)
+
+// newDate=Date()
+// console.log(newDate)
+// throw new Error("only values 1 - 10 are permitted")
